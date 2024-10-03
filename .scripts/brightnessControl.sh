eeaa774ef1eef31b71ac3d982a0a3e08bfcaa6ea
Emmanuel Butsana <ebutsana@gmail.com>
2024-10-03 11:01:24 -0400

    Updated scripts

--- .scripts/brightnessControl.sh
+++ .scripts/brightnessControl.sh
@@ -4,12 +4,12 @@
 action=$1
 
 # Attempt to increase brightness on AMD, else try Nvidia
-if [ "$action" == "increase" ]; then
+if [[ "$action" == "increase" ]]; then
   brightnessctl -q -d amdgpu_bl2 set +5% >/dev/null 2>&1 || \
     brightnessctl -q -d nvidia_0 set +5% >/dev/null 2>&1
 
 # Attempt to decraese brightness on AMD, else try Nvidia
-elif [ "$action" == "decrease" ]; then
+elif [[ "$action" == "decrease" ]]; then
   brightnessctl -q -d amdgpu_bl2 set 5%- >/dev/null 2>&1 || \
     brightnessctl -q -d nvidia_0 set 5%- >/dev/null 2>&1
 
